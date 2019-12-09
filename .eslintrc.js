6d1c2eb78dacf14242d3fd3638e8b1c0ae92ab1d
bug Maker <451354@qq.com>
2019-12-09 23:14:14 +0800

    add: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: ['error', 'never'],
+  },
+};

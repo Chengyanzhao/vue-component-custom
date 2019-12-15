4901a605ebc67bfe17c7184440e872d159d66792
bug Maker <451354@qq.com>
2019-12-15 21:21:01 +0800

    change: rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/recommended',
-    'airbnb-base',
+    // 'airbnb-base',
     'prettier',
   ],
   parserOptions: {
@@ -18,5 +18,21 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: ['error', 'never'],
+    'vue/no-v-html': 'off',
+    "vue/require-prop-types": "off",
+    "vue/require-default-prop": "off",
+    "vue/no-reserved-keys": "off",
+    "vue/prop-name-casing": "off",
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 1,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": true
+      }
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }]
   },
 };
